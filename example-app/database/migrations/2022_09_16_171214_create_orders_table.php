38bbe13b2988772d2eb6d402af68319ccccfc167
igor-roganov1989 <30695538+igor-roganov1989@users.noreply.github.com>
2022-09-23 16:08:13 +0300

    example-app

---
+++ example-app/database/migrations/2022_09_16_171214_create_orders_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')
+                ->references('id')
+                ->table('users');
+
+            $table->string('user_city');
+            $table->string('user_country');
+            $table->string('user_adress');
+
+            $table->text('order_comment');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
